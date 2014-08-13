2e4e9ef3cef5fd99964e9fc2440fa9c2e692390d
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-08-13 16:20:11 +0200

    updating game data

--- src/Statics.js
+++ src/Statics.js
@@ -12,14 +12,15 @@ var clickAreas = {
 
 var GameData = {
   settings: {},
-  seashells: 0,
-  oysters:   0,
-  driftwood: 0
   player: {
-    hitpoints: 20,
-    magic:     0,
+    hp:        20,
+    mp:        0,
     armor:     null,
     weapon:    null,
-    skills: []
+    inventory: [],
+    skills: {
+      specials: [],
+      magic: []
+    }
   }
 }
