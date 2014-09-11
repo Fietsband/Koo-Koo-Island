b86235ebc8b07840de1cf3eeca60ee57c7d59b79
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-09-11 18:38:10 +0200

    updating enemy class - adding auto attack sequence - adding healthbar

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,15 +1,59 @@
 Enemy.prototype = {
+  startAttacking: function(){
+    var self = this;
+    this.attackInterval = setInterval(function(){
+      var toPerformAttack = self.pickAttack();
+      self.attackWith(toPerformAttack);
+    }, this.enemyInformation.attack_interval);
+  },
+
+  quitAttacking: function(){
+    clearInterval(this.attackInterval);
+  },
+
+  pickAttack: function(){
+    var attacks = [];
+    for(var attack in this.enemyInformation.attacks){
+      for(var i = 0; i < this.enemyInformation.attacks[attack].probability; i++){
+        attacks.push(this.enemyInformation.attacks[attack]);
+      }
+    }
+    picked_attack = Math.round(Math.random() * attacks.length);
+    return attacks[picked_attack];
+  },
+
+  attackWith: function(attack){
+    window.Game.player.looseHealth(attack.damage);
+  },
+
   getEnemyStats: function(){
-    return Enemies[identifier];
+    return Enemies[this.identifier];
+  },
+
+  looseHealth: function(amount){
+    this.enemyInformation.health -= amount;
+    if(this.enemyInformation.health <= 0){
+      this.enemyInformation.health = 0;
+    }
+    this.updateHealthBar();
+  },
+
+  updateHealthBar: function(){
+    this.healthBar.style.width = this.currentHealth() + "%";
+  },
+
+  currentHealth: function(){
+    return (this.enemyInformation.health / this.startHealth) * 100;
   }
 }
 
-function Enemy(identifier){
-  this.identifier = identifier;
-  this.enemyPre = document.querySelector(".enemies #" + identifier);
-  this.enemyInformation = this.getEnemyStats();
-  // build timer
-  // attack
-  // health
-  // die!
+function Enemy(identifier, callbacks){
+  this.identifier         = identifier;
+  this.callbacks          = callbacks;
+  this.enemyPre           = document.querySelector(".enemies #" + identifier);
+  this.healthBar          = document.querySelector(".field .enemy .healthbar .health-left");
+  this.enemyInformation   = this.getEnemyStats();
+  this.startHealth        = this.enemyInformation.health;
+  this.attackInterval     = null;
+  this.updateHealthBar();
 }
