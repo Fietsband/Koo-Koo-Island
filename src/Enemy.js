1ab0133165999b3110bbabe4f2f4d00d20754641
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-09-14 13:00:54 +0200

    fixing end battle + enemy picker

--- src/Enemy.js
+++ src/Enemy.js
@@ -19,7 +19,7 @@ Enemy.prototype = {
         attacks.push(this.enemyInformation.attacks[attack]);
       }
     }
-    picked_attack = Math.round(Math.random() * attacks.length);
+    picked_attack = Math.round(Math.random() * (attacks.length - 1));
     return attacks[picked_attack];
   },
 
@@ -36,32 +36,58 @@ Enemy.prototype = {
   looseHealth: function(amount){
     this.enemyInformation.health -= amount;
     if(this.enemyInformation.health <= 0){
-      this.enemyInformation.health = 0;
-      window.currentBattle.pause();
-      setTimeout(function(){
-        window.currentBattle.battlePopup.hide();
-      }, 2000);
+      window.currentBattle.endBattle(this.enemyInformation.health, function(){
+        window.currentBattle.infoHeader.update("You've beaten " + this.enemyInformation.name);
+      });
     }
     this.updateHealthBar();
   },
 
   updateHealthBar: function(){
     this.healthBar.style.width = this.currentHealth() + "%";
+    this.healthStats.innerHTML = this.enemyInformation.health;
   },
 
   currentHealth: function(){
     return (this.enemyInformation.health / this.startHealth) * 100;
+  },
+
+  getGraphic: function(){
+    return this.enemyPre.innerHTML;
+  },
+
+  setGraphicToBattleField: function(){
+    this.enemyPlaceholder  = document.querySelector("#battle-sequence-popup .field .enemy #graphic");
+    this.enemyPlaceholder.innerHTML = this.getGraphic();
+  },
+
+  setPosition: function(){
+    this.enemyPosition = document.querySelector("#battle-sequence-popup .field .enemy");
+    this.enemyPosition.style.top = this.enemyInformation.position.y + "px";
+    this.enemyPosition.style.right = this.enemyInformation.position.x + "px";
+  },
+
+  spawn: function(){
+    this.setPosition();
+    this.setGraphicToBattleField();
+  },
+
+  unspawn: function(){
+    this.enemyPlaceholder.innerHTML = "";
   }
 }
 
 function Enemy(identifier, callbacks){
-  this.identifier         = identifier;
-  this.callbacks          = callbacks;
-  this.enemyPre           = document.querySelector(".enemies #" + identifier);
-  this.healthBar          = document.querySelector(".field .enemy .healthbar .health-left");
-  this.enemyInformation   = this.getEnemyStats();
-  this.startHealth        = this.enemyInformation.health;
-  this.attackHoldBar      = new Bar(".field .enemy .attackbar .attack-left", this.enemyInformation.attack_interval);
-  this.attackInterval     = null;
+  this.identifier             = identifier;
+  this.callbacks              = callbacks;
+  this.enemyPre               = document.querySelector(".enemies #" + identifier);
+  this.healthBar              = document.querySelector(".field .enemy .healthbar .health-left");
+  this.healthStats            = document.querySelector(".field .enemy .health-stats .health-stats-left");
+  this.totalHealth            = document.querySelector(".field .enemy .health-stats .total-health");
+  this.enemyInformation       = this.getEnemyStats();
+  this.startHealth            = this.enemyInformation.health;
+  this.totalHealth.innerHTML  = this.startHealth;
+  this.attackHoldBar          = new Bar(".field .enemy .attackbar .attack-left", this.enemyInformation.attack_interval);
+  this.attackInterval         = null;
   this.updateHealthBar();
 }
