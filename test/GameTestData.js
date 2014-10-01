491ebcc1f2b78139bbfcfb7dbfd63fd0c727581e
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-01 17:10:56 +0200

    updating game test data

---
+++ test/GameTestData.js
@@ -0,0 +1,29 @@
+var GameData = {
+  settings: {},
+  player: {
+    hp:               20,
+    mp:               0,
+    experience:       0,
+    attack_damage:    10,
+    battle_timeout:   5000,
+    seashells:        0,
+    oysters:          0,
+    woods:            0,
+    armor:            null,
+    weapon:           null,
+    inventory:        {
+      weapons:          [],
+      skills:           [],
+      magic:            [],
+      items:            []
+    }
+  },
+
+  progress: {
+    show_fish: 1,
+    show_bottle: 1,
+    show_shark: 0,
+    show_build_bridge_button: 1,
+    enable_build_bridge_button: 1,
+  }
+}
