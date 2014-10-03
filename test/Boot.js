d495d1c4753c2e7d002ff3eef7cd2f004bf11fa3
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-03 13:35:31 +0200

    updating tests

--- test/Boot.js
+++ test/Boot.js
@@ -1,10 +1,21 @@
 function boot(){
-  $.domReady(function(){
-    window.Game = new Game();
-    window.Game.initialize();
-    window.Stats = new Stats();
-    initializeAutoSaveButton();
-    initializeSaveButton();
-    initializeLoadButton();
-  });
+  $.domReady(setup);
+}
+
+function setup(){
+  console.log(window.GameData.player.inventory.items);
+  console.log("booty call");
+  console.log(window.Game);
+  window.currentGame = new Game();
+  window.currentGame.initialize();
+  window.currentStats = new Stats();
+  initializeAutoSaveButton();
+  initializeSaveButton();
+  initializeLoadButton();
+}
+
+function resetGame(){
+  delete window.currentGame;
+  delete window.currentStats;
+  setup();
 }
--- test/tests/MainTest.js
+++ test/tests/MainTest.js
@@ -1,26 +1,34 @@
 boot();
 
 QUnit.test("0.0 game initialization", function(assert){
-  assert.ok(window.Game, "Passed!");
+  assert.ok(window.currentGame, "Passed!");
 });
 
 QUnit.test("0.1 game player initialization", function(assert){
-  assert.ok(window.Game.player, "There should be player attached to the game");
-  assert.ok(window.Game.player.inventory, "The player should have an inventory");
-  assert.ok(window.Game.player.inventory.getInventory().items, "Passed!");
-  assert.ok(window.Game.player.inventory.getInventory().weapons, "Passed!");
-  assert.ok(window.Game.player.inventory.getInventory().magic, "Passed!");
+  assert.ok(window.currentGame.player, "There should be player attached to the game");
+  assert.ok(window.currentGame.player.inventory, "The player should have an inventory");
+  assert.ok(window.currentGame.player.inventory.getInventory().items, "Passed!");
+  assert.ok(window.currentGame.player.inventory.getInventory().weapons, "Passed!");
+  assert.ok(window.currentGame.player.inventory.getInventory().magic, "Passed!");
 });
 
 QUnit.test("0.2 game levels initialization", function(assert){
-  assert.ok(window.Game.levels, "Passed");
-  assert.ok(window.Game.levels.island, "Passed");
-  assert.ok(window.Game.levels.squirrel_city, "Passed");
-  assert.ok(window.Game.levels.squirrel_city_first_level, "Passed");
-  assert.ok(window.Game.levels.squirrel_city_second_level, "Passed");
-  assert.ok(window.Game.levels.squirrel_city_attic_level, "Passed");
+  assert.ok(window.currentGame.levels, "Passed");
+  assert.ok(window.currentGame.levels.island, "Passed");
+  assert.ok(window.currentGame.levels.squirrel_city, "Passed");
+  assert.ok(window.currentGame.levels.squirrel_city_first_level, "Passed");
+  assert.ok(window.currentGame.levels.squirrel_city_second_level, "Passed");
+  assert.ok(window.currentGame.levels.squirrel_city_attic_level, "Passed");
 });
 
 QUnit.test("0.3 stats should be initialized", function(assert){
-  assert.ok(window.Stats, "Passed");
+  assert.ok(window.currentStats, "Passed");
 });
+
+QUnit.test("loading data", function(assert){
+  GameData.player.inventory.items.push({itemTitle: "map", identifier: "map"});
+
+  resetGame();
+
+  assert.ok(window.currentGame.player.inventory.getInventory().items[0].itemTitle, "map", "should add map to inventory");
+})
