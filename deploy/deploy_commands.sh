9c675db90eeb61ad49fd691bca27da9acb2ae4b1
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-10-07 17:10:30 +0200

    updating deploy commands

--- deploy/deploy_commands.sh
+++ deploy/deploy_commands.sh
@@ -1,4 +1,16 @@
+echo "> Starting deploy"
 cd /www/
 rm -rf Koo-Koo-Island
+echo "> Removed old project"
+
 git clone https://github.com/grdw/Koo-Koo-Island.git
+cd Koo-Koo-Island
+
+find src/ -type f | grep -v application.min.js | xargs rm
+echo "> Removing source files"
+
+find src/ -type d -empty | xargs rm -rf
+echo "> Removing empty dirs"
+
 cache-purge http://grdw.nl
+echo "> Finished"
