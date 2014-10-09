cd517347262e98ab02aaa6015925ae833025ebf7
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-10-09 17:13:15 +0200

    grab spear succesfully from wall + replacing game with currentGame

--- src/levels/SquirrelCity.js
+++ src/levels/SquirrelCity.js
@@ -10,24 +10,27 @@ SquirrelCity = {
   },
 
   goToFirstLevel: function(){
-    window.Game.levels.squirrel_city_second_level.addToGame();
+    window.currentGame.levels.squirrel_city_second_level.addToGame();
   },
 
   goToSecondLevel: function(){
-    window.Game.levels.squirrel_city_attic_level.addToGame();
+    window.currentGame.levels.squirrel_city_attic_level.addToGame();
   },
 
   goToGroundLevel: function(){
-    window.Game.levels.squirrel_city_first_level.addToGame();
+    window.currentGame.levels.squirrel_city_first_level.addToGame();
   },
 
   goOutside: function(){
-    window.Game.levels.squirrel_city.addToGame();
+    window.currentGame.levels.squirrel_city.addToGame();
   },
 
   grabSpear: function(){
-    var item = new Weapon();
-    window.Game.player.inventory.addItem(item);
-    window.Game.levels.squirrel_city_second_level.levelClickAreas[".squirrel-city-spear-wall"].disable();
+    var item = new Weapon("spear");
+    window.currentGame.player.inventory.addItem("weapons", item);
+    window.currentGame.levels.squirrel_city_second_level.levelClickAreas[".squirrel-city-spear-wall"].disable();
+    $.each(document.querySelectorAll(".squirrel-city-spear-wall"), function(i, spearPart){
+      spearPart.innerHTML = "&nbsp;";
+    });
   }
 }
