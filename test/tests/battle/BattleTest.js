d6a0081d9049b0f7d09c35a9770171d2dc29de39
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-10-09 16:08:24 +0200

    updating battle tests

--- test/tests/battle/BattleTest.js
+++ test/tests/battle/BattleTest.js
@@ -5,6 +5,9 @@ QUnit.test("should start a new battle", function(assert){
 
   assert.deepEqual(window.currentBattle, battle, "current battle should be battle");
   assert.deepEqual(window.currentBattle.enemy, enemy, "current battle enemy should be an enemy");
+  assert.ok(window.currentBattle.battleEngine, "current battle engine should be there");
+  assert.ok(window.currentBattle.infoHeader, "current battle info header should be there");
+  assert.ok(window.currentBattle.battlePopup, "current battle popup should be there");
 
   battle.battlePopup.hide();
 });
