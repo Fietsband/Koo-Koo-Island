0f51424dd800b7e8658020197f12a082f1480ac4
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-12 16:11:42 +0200

    adding click handler for whirlpool

--- test/tests/levels/LonelyIslandTest.js
+++ test/tests/levels/LonelyIslandTest.js
@@ -5,3 +5,10 @@ QUnit.test("disable seashell", function(assert){
   assert.equal(document.querySelector("#game-levels #island #island-seashell").innerHTML, "&nbsp;", "there should be no graphic attached to the seashell");
   assert.equal(GameData.progress.hide_seashell, 1, 'should hide seashell to 1');
 });
+
+QUnit.test("enabling whirpool", function(assert){
+  window.LonelyIsland.enableWhirlpool();
+
+  assert.ok(document.querySelector("#game-levels #island #island-whirlpool").onclick !== null, "should have a click handler");
+  assert.equal(document.querySelector("#game-levels #island #island-whirlpool").innerHTML, "≋", "should show whirlpool");
+});
