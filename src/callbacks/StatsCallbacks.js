2479e80b9d194c152df8eeb254125a7594c82792
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-12 15:52:55 +0200

    disabling seashell when the counter reaches 200

--- src/callbacks/StatsCallbacks.js
+++ src/callbacks/StatsCallbacks.js
@@ -7,6 +7,9 @@ StatsCallbacks = {
       else if(amount > 19 && !window.currentGame.checkProgressOn('show_bottle')){
         window.LonelyIsland.showMessageInABottle();
       }
+      else if(amount > 199 && !window.currentGame.checkProgressOn('hide_seashell')){
+        window.LonelyIsland.disableSeashell();
+      }
       else if(amount > 999 && !window.currentGame.checkProgressOn('show_shark')){
         window.LonelyIsland.addShark();
       }
