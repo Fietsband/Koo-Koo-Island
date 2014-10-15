a35cabb7080983a8f8fcc37facfdd59b4735a8f7
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-15 21:57:33 +0200

    adding stat bar

---
+++ src/interaction/StatBar.js
@@ -0,0 +1,26 @@
+StatBar.prototype = {
+  update: function(amount){
+    this.currentStatAmount       = amount;
+    this.currentAmount.innerHTML = amount;
+    this.innerBar.style.width    = this.calculatePercentage() + "%";
+  },
+
+  initialize: function(currentStatAmount, totalStatAmount){
+    this.currentStatAmount       = currentStatAmount;
+    this.totalStatAmount         = totalStatAmount;
+    this.currentAmount.innerHTML = currentStatAmount;
+    this.total.innerHTML         = totalStatAmount;
+    this.update(currentStatAmount);
+  },
+
+  calculatePercentage: function(){
+    return (this.currentStatAmount / this.totalStatAmount) * 100;
+  }
+};
+
+function StatBar(barScope, currentAmount, total, innerBar){
+  this.barScope       = document.querySelector(barScope);
+  this.currentAmount  = this.barScope.querySelector(currentAmount);
+  this.total          = this.barScope.querySelector(total);
+  this.innerBar       = this.barScope.querySelector(innerBar);
+}
