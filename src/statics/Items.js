dda6db7aaeef44f8c9085949d54a5171626fa27b
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-17 08:13:04 +0200

    removing #

--- src/statics/Items.js
+++ src/statics/Items.js
@@ -1,35 +1,35 @@
 window.Items = {
   potion: {
     use: function(){
-      document.getElementById("#potion").remove();
+      document.getElementById("potion").remove();
       window.currentGame.player.increaseHealth(25);
     }
   },
 
   hiPotion: {
     use: function(){
-      document.getElementById("#hiPotion").remove()
+      document.getElementById("hiPotion").remove()
       window.currentGame.player.increaseHealth(100);
     }
   },
 
   ether: {
     use: function(){
-      document.getElementById("#ether").remove()
+      document.getElementById("ether").remove()
       window.currentGame.player.increaseMagic(15);
     }
   },
 
   hiEther: {
     use: function(){
-      document.getElementById("#hiEther").remove()
+      document.getElementById("hiEther").remove()
       window.currentGame.player.increaseMagic(50);
     }
   },
 
   elixer: {
     use: function(){
-      document.getElementById("#elixer").remove()
+      document.getElementById("elixer").remove()
       window.currentGame.player.increaseHealth(GameData.player.hp[1]);
       window.currentGame.player.increaseMagic(GameData.player.mp[1]);
     }
