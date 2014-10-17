c4de31338d165e0977378272aa48f9dcae1f980f
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-10-17 16:53:13 +0200

    giving items a title

--- src/statics/Items.js
+++ src/statics/Items.js
@@ -1,5 +1,6 @@
 window.Items = {
   potion: {
+    title: "Potion",
     use: function(){
       document.getElementById("potion").remove();
       window.currentGame.player.increaseHealth(25);
@@ -7,6 +8,7 @@ window.Items = {
   },
 
   hiPotion: {
+    title: "Hi-Potion",
     use: function(){
       document.getElementById("hiPotion").remove()
       window.currentGame.player.increaseHealth(100);
@@ -14,6 +16,7 @@ window.Items = {
   },
 
   ether: {
+    title: "Ether",
     use: function(){
       document.getElementById("ether").remove()
       window.currentGame.player.increaseMagic(15);
@@ -21,6 +24,7 @@ window.Items = {
   },
 
   hiEther: {
+    title: "Hi-Ether",
     use: function(){
       document.getElementById("hiEther").remove()
       window.currentGame.player.increaseMagic(50);
@@ -28,6 +32,7 @@ window.Items = {
   },
 
   elixer: {
+    title: "Elixer",
     use: function(){
       document.getElementById("elixer").remove()
       window.currentGame.player.increaseHealth(GameData.player.hp[1]);
@@ -36,6 +41,7 @@ window.Items = {
   },
 
   map: {
-
+    title: "A partial map of the world",
+    description: "A partial map of the world"
   }
 };
