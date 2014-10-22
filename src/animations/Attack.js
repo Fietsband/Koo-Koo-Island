b40f1ffa3d8936831936f043ccb049050c8e99d7
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-22 20:24:10 +0200

    updating attack animation

--- src/animations/Attack.js
+++ src/animations/Attack.js
@@ -1,9 +1,10 @@
 window.Animations.Attack = {
   play: function(graphic){
+    var player = window.currentGame.player;
     var enemy = window.currentBattle.enemy;
     var clearMove = null;
 
-    move(window.currentGame.player.battleGraphic)
+    move(player.battleGraphic)
       .x(enemy.enemyInformation.player_position.x)
       .y(enemy.enemyInformation.player_position.y)
       .duration('0.4s')
@@ -13,13 +14,13 @@ window.Animations.Attack = {
     clearTimeout(clearMove);
 
     clearMove = setTimeout(function(){
-      move(window.currentGame.player.battleGraphic)
+      move(player.battleGraphic)
         .x(0)
         .duration('0.3s')
         .ease('out')
         .end(function(){
           window.currentBattle.controls.enable.bind(window.currentBattle)();
-          enemy.looseHealth(GameData.player.attack_damage);
+          enemy.looseHealth(player.calculateDamage());
         });
 
       clearTimeout(clearMove);
