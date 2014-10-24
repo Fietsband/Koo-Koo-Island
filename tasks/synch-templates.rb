6567c6849b9f8d5e88f396ca26041e99ea3acf82
Gerard Westerhof <gerard.westerhof@holder.nl>
2014-10-24 14:45:34 +0200

    updating template synch script

--- tasks/synch-templates.rb
+++ tasks/synch-templates.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
-template = File.read("template.html")
+LAYOUTS          = %w(development.html test/index.html index.html)
+TEMPLATE         = File.read("template.html")
+LAYOUT_REGEX     = /(<\!--\*\*BEGIN\*\*-->)((\n|.)*)(<!--\*\*END\*\*-->)/
+TEMPLATE_REPLACE = "\\1\n#{TEMPLATE}    \\4"
 
-["development.html", "test/index.html", "index.html"].each do |t|
-  html_file = File.read(t)
-  html_file.gsub!(/(<\!--\*\*BEGIN\*\*-->)((\n|.)*)(<!--\*\*END\*\*-->)/, "\\1\n#{template}    \\4")
-
-  File.open(t, 'w') do |out|
-    out << html_file
+def set_layout(layout, html_file)
+  File.open(layout, 'w') do |f|
+    f << html_file
   end
+end
+
+LAYOUTS.each do |layout|
+  set_layout layout, File.read(layout).gsub!(LAYOUT_REGEX, TEMPLATE_REPLACE)
 end
