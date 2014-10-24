c9aa971d4950b1dacc331384cc986a43194945fa
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-24 10:20:26 +0200

    updating click areas

--- src/statics/ClickAreas.js
+++ src/statics/ClickAreas.js
@@ -1,104 +1,31 @@
-var clickAreas = {
+window.ClickAreas = {
   island: [
-    {
-      id: "#island-seashell",
-      note: "First shell where you can click on",
-      klass: "currentStats",
-      method: "increaseStat",
-      args: ["seashell", 1, StatsCallbacks.seashell.performCallback]
-    },
-
-    {
-      id: "#island-bridge",
-      note: "When you buy your first wood you can build a bridge to the other side"
-    },
-
-    {
-      id: "#battle-big-shark",
-      note: "When something happens ?!",
-      klass: "BattleInitializer",
-      method: "start",
-      args: ["big-shark"]
-    },
-
-    {
-      id: "#island-whirlpool",
-      note: "When the magical seashell is gone",
-      klass: "UnderwaterShack",
-      method: function(){
-        window.currentGame.levels.underwater_shack.addToGame();
-      }
-    }
+    "#island-seashell",
+    "#island-bridge",
+    "#battle-big-shark",
+    "#island-whirlpool"
   ],
 
   underwater_shack: [
-    {
-      id: "#island-whirlpool",
-      note: "when you click on it you go back to the island"
-    }
+    "#island-whirlpool"
   ],
 
   squirrel_city: [
-    {
-      id: "#mysterious-squirrel",
-      note: "A mysterious squirrel"
-    },
-    {
-      id: "#haunted-house-door",
-      note: "A mysterious squirrel",
-      klass: "SquirrelCity",
-      method: "openDoor"
-    }
+    "#mysterious-squirrel",
+    "#haunted-house-door"
   ],
 
-  squirrel_city_first_level_house: [
-    {
-      id: "#squirrel-city-goto-first-level",
-      note: "go to first level",
-      klass: "SquirrelCity",
-      method: "goToFirstLevel"
-    },
-
-    {
-      id: ".door-squirrel-city-house",
-      note: "go outside",
-      klass: "SquirrelCity",
-      method: "goOutside"
-    }
+  squirrel_city_first_level_house:  [
+    "#squirrel-city-goto-first-level"
   ],
 
   squirrel_city_second_level_house: [
-    {
-      id: "#squirrel-city-goto-second-level",
-      note: "go to second level",
-      klass: "SquirrelCity",
-      method: "goToSecondLevel"
-    },
-
-    {
-      id: "#squirrel-city-goto-ground-level",
-      note: "go to ground level",
-      klass: "SquirrelCity",
-      method: "goToGroundLevel"
-    },
-
-    {
-      id: ".squirrel-city-spear-wall",
-      note: "grab spear on wall",
-      klass: "SquirrelCity",
-      method: "grabSpear",
-      isDisabled: function(){
-        return window.currentGame.checkProgressOn('squirrel_house_spear');
-      }
-    }
+    "#squirrel-city-goto-second-level",
+    "#squirrel-city-goto-ground-level",
+    ".squirrel-city-spear-wall"
   ],
 
-  squirrel_city_attic_level_house: [
-    {
-      id: "#squirrel-city-goto-first-level-back",
-      note: "go to first level",
-      klass: "SquirrelCity",
-      method: "goToFirstLevel"
-    }
+  squirrel_city_attic_level_house:  [
+    "#squirrel-city-goto-first-level-back"
   ]
-}
+};
