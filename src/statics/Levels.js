6c0773675a0526837755412854a64220d260d4d2
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-24 09:23:42 +0200

    updating levels static

--- src/statics/Levels.js
+++ src/statics/Levels.js
@@ -1,8 +1,25 @@
 window.Levels = {
-  island:                     "LonelyIsland",
-  underwater_shack:           "UnderwaterShack",
-  squirrel_city:              "SquirrelCity",
-  squirrel_city_first_level:  null,
-  squirrel_city_second_level: "SquirrelCitySecondLevel",
-  squirrel_city_attic_level:  null
+  island: {
+    module: "LonelyIsland", clicks: "LonelyIslandClicks"
+  },
+
+  underwater_shack:{
+    module: "UnderwaterShack"
+  },
+
+  squirrel_city:{
+    module: "SquirrelCity"
+  },
+
+  squirrel_city_first_level:{
+    module: null
+  },
+
+  squirrel_city_second_level:{
+    module: "SquirrelCitySecondLevel"
+  },
+
+  squirrel_city_attic_level:{
+    module: null
+  }
 }
