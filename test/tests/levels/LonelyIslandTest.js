d19bc0da5a4fdde1652bf0bcfb26af17201ab44c
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-25 09:17:15 +0200

    updating lonely island test

--- test/tests/levels/LonelyIslandTest.js
+++ test/tests/levels/LonelyIslandTest.js
@@ -1,14 +1,14 @@
 QUnit.test("disable seashell", function(assert){
   window.LonelyIsland.disableSeashell();
 
-  assert.equal(dom.find("#game-levels #island #island-seashell").onclick, null, "there should be no method attached to the click handler");
-  assert.equal(dom.find("#game-levels #island #island-seashell").innerHTML, "&nbsp;", "there should be no graphic attached to the seashell");
+  assert.equal(dom.find("#game-levels #lonely_island #island-seashell").onclick, null, "there should be no method attached to the click handler");
+  assert.equal(dom.find("#game-levels #lonely_island #island-seashell").innerHTML, "&nbsp;", "there should be no graphic attached to the seashell");
   assert.equal(GameData.progress.hide_seashell, 1, 'should hide seashell to 1');
 });
 
 QUnit.test("enabling lonely island whirlpool", function(assert){
   window.LonelyIsland.enableWhirlpool();
 
-  assert.ok(dom.find("#game-levels #island #island-whirlpool").onclick !== null, "should have a click handler");
-  assert.equal(dom.find("#game-levels #island #island-whirlpool").innerHTML, "≋", "should show whirlpool");
+  assert.ok(dom.find("#game-levels #lonely_island #island-whirlpool").onclick !== null, "should have a click handler");
+  assert.equal(dom.find("#game-levels #lonely_island #island-whirlpool").innerHTML, "≋", "should show whirlpool");
 });
