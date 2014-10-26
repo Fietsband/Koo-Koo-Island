5a1ff8c17fce5ca9a582f3022850fdffd618838b
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-26 18:04:46 +0100

    adding test for death of evil-portrait

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,7 +1,43 @@
 window.Test = {};
 
 window.Test.resetStats = function(){
-  GameData.player.seashells = 0;
-  GameData.player.oysters   = 0;
-  GameData.player.woods     = 0;
+  GameData = {
+    settings: {},
+    player: {
+      hp:               [20, 20],
+      mp:               [0, 0],
+      experience:       0,
+      attack_damage:    10,
+      battle_timeout:   5000,
+      seashells:        0,
+      oysters:          0,
+      woods:            0,
+      armor:            'none',
+      weapon:           'none',
+      map:              1,
+      inventory:        {
+        weapons:          [],
+        armors:           [],
+        skills:           [],
+        magic:            [],
+        items:            []
+      },
+      experience_rewards: []
+    },
+
+    progress: {
+      show_fish:                     0,
+      show_bottle:                   0,
+      show_shark:                    0,
+      show_build_bridge_button:      0,
+      enable_build_bridge_button:    0,
+      hide_seashell:                 0,
+      beaten_shark:                  0,
+      squirrel_house_spear:          0,
+      squirrel_house_small_painting: 0,
+      squirrel_house_closet:         0,
+      squirrel_house_boss_fight:     0,
+      squirrel_house_chimney:        0
+    }
+  }
 };
--- test/tests/levels/SquirrelCityAtticLevelHouseTest.js
+++ test/tests/levels/SquirrelCityAtticLevelHouseTest.js
@@ -18,4 +18,26 @@ QUnit.module("initializing the attic level", {
     assert.ok(dom.findId("open-inventory-button").disabled);
   });
 
+QUnit.module("reward player with a part of the map", {
+  setup: function(){
+    window.Test.enemy  = new Enemy("evil-portrait");
+    window.Test.battle = new Battle(window.Test.enemy);
+    window.Test.battle.start();
+    window.Test.enemy.looseHealth(100);
+    window.currentBattle.eventEngine.invoke();
+  },
+
+  teardown: function(){
+    window.Test.battle.battlePopup.hide();
+    window.currentGame.gameMap.popUp.hide();
+    GameData.player.inventory.items = [];
+    window.Test.resetStats();
+  }
+});
+
+  QUnit.test("should set the boss fight to 1", function(assert){
+
+    assert.ok(window.currentGame.checkProgressOn("squirrel_house_boss_fight"));
+  });
+
 QUnit.module();
