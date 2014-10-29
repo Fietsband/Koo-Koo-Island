2d78f81d2095bf754558faeac311cb24f92a3391
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-10-29 21:34:12 +0100

    adding private methods where possible

--- src/Level.js
+++ src/Level.js
@@ -1,103 +1,103 @@
-Level.prototype = {
-  addToGame: function(){
-    this.unsetCurrentGame();
-    this.setCurrentGame();
-  },
+var Level = (function(){
+  Level.prototype = {
+    addToGame: function(){
+      this.unsetCurrentGame();
+      this.setCurrentGame();
+    },
 
-  setCurrentGame: function(){
-    this.showMap();
-    this.setClickMethodsToLevel();
-    this.performInitialize();
-    window.currentGame.gameMap.disableMapSpot(this.mapSpot);
-    window.currentLevel = this;
-  },
+    setCurrentGame: function(){
+      this.showMap();
+      this.setClickMethodsToLevel();
+      this.performInitialize();
+      window.currentGame.gameMap.disableMapSpot(this.mapSpot);
+      window.currentLevel = this;
+    },
 
-  unsetCurrentGame: function(){
-    if(window.currentLevel){
-      window.currentLevel.hideMap();
-      window.currentLevel.removeClickAreas();
-      window.currentLevel.destroy();
-      window.currentGame.gameMap.enableMapSpot(window.currentLevel.mapSpot);
-      window.currentLevel = null;
-      delete window.currentLevel;
+    unsetCurrentGame: function(){
+      if(window.currentLevel){
+        window.currentLevel.hideMap();
+        window.currentLevel.removeClickAreas();
+        window.currentLevel.destroy();
+        window.currentGame.gameMap.enableMapSpot(window.currentLevel.mapSpot);
+        window.currentLevel = null;
+        delete window.currentLevel;
+      }
+    },
+
+    showMap: function(){
+      this.levelDom.style.display = "block";
+    },
+
+    hideMap: function(){
+      this.levelDom.style.display = "none";
+    },
+
+    removeClickAreas: function(){
+      this.clickArea = [];
+    },
+
+    setClickMethodsToLevel: function(){
+      $.each(this.clickAreas, setClickMethodToArea.bind(this));
+    },
+
+    performInitialize: function(){
+      if(this.levelModule && this.levelModule["initialize"]){
+        this.levelModule["initialize"]();
+      }
+    },
+
+    destroy: function(){
+      if(this.levelModule && this.levelModule["destroy"]){
+        this.levelModule["destroy"]();
+      }
     }
-  },
+  }
 
-  showMap: function(){
-    this.levelDom.style.display = "block";
-  },
-
-  hideMap: function(){
-    this.levelDom.style.display = "none";
-  },
-
-  removeClickAreas: function(){
-    this.clickArea = [];
-  },
-
-  setClickMethodsToLevel: function(){
-    $.each(this.clickAreas, this.setClickMethodToArea.bind(this));
-  },
-
-  setClickMethodToArea: function(i, area){
+  function setClickMethodToArea(i, area){
     var clickMethod = $.methodize(area);
     new ClickArea(area, this.clickMethods[clickMethod]).toggle();
-  },
+  }
 
-  setMapSpot: function(){
-    return window.currentGame.gameMap.findMapSpot(this.mapSpot);
-  },
-
-  setLevelModule: function(){
-    if(window[this.moduleName()]){
-      return window[this.moduleName()];
+  function setClickMethods(){
+    if(window.LevelClickMethods[this.moduleName]){
+      return window.LevelClickMethods[this.moduleName];
     }
     else{
       return {};
     }
-  },
+  }
 
-  moduleName: function(){
-    return $.constantize(this.identifier);
-  },
-
-  setClickMethods: function(){
-    if(window.LevelClickMethods[this.moduleName()]){
-      return window.LevelClickMethods[this.moduleName()];
-    }
-    else{
-      return {};
-    }
-  },
-
-  setClickAreas: function(){
+  function setClickAreas(){
     if(window.ClickAreas[this.identifier]){
       return window.ClickAreas[this.identifier];
     }
     else{
       return [];
     }
-  },
+  }
 
-  performInitialize: function(){
-    if(this.levelModule && this.levelModule["initialize"]){
-      this.levelModule["initialize"]();
+  function setLevelModule(){
+    if(window[this.moduleName]){
+      return window[this.moduleName];
     }
-  },
-
-  destroy: function(){
-    if(this.levelModule && this.levelModule["destroy"]){
-      this.levelModule["destroy"]();
+    else{
+      return {};
     }
   }
-}
 
-function Level(identifier, mapSpot){
-  this.identifier  = identifier;
-  this.mapSpot     = mapSpot || identifier;
-  this.levelDom    = dom.find("#game-levels #" + identifier);
-  this.pointOnMap  = this.setMapSpot();
-  this.levelModule = this.setLevelModule();
-  this.clickAreas  = this.setClickAreas();
-  this.clickMethods= this.setClickMethods();
-}
+  function setMapSpot(){
+    return window.currentGame.gameMap.findMapSpot(this.mapSpot);
+  }
+
+  function Level(identifier, mapSpot){
+    this.identifier  = identifier;
+    this.mapSpot     = mapSpot || identifier;
+    this.levelDom    = dom.find("#game-levels #" + identifier);
+    this.moduleName  = $.constantize(identifier);
+    this.pointOnMap  = setMapSpot.bind(this)();
+    this.levelModule = setLevelModule.bind(this)();
+    this.clickAreas  = setClickAreas.bind(this)();
+    this.clickMethods= setClickMethods.bind(this)();
+  }
+  return Level;
+})();
--- src/Player.js
+++ src/Player.js
@@ -1,143 +1,146 @@
-Player.prototype = {
-  buy: function(item){
-    var sellPrice = sellPrices[item];
-    if(GameData.player[sellPrice.type] >= sellPrice.value){
-      var sellType = sellPrice.type.substring(0, sellPrice.type.length - 1);
-      window.currentStats.increaseStat(item, 1);
-      window.currentStats.increaseStat(sellType, (sellPrice.value * -1));
-    }
-  },
+var Player = (function(){
+  Player.prototype = {
+    buy: function(item){
+      var sellPrice = sellPrices[item];
+      if(GameData.player[sellPrice.type] >= sellPrice.value){
+        var sellType = sellPrice.type.substring(0, sellPrice.type.length - 1);
+        window.currentStats.increaseStat(item, 1);
+        window.currentStats.increaseStat(sellType, (sellPrice.value * -1));
+      }
+    },
 
-  canBuyItem: function(item){
-    var playerInventoryItemCount = GameData.player[sellPrices[item].type];
-    return playerInventoryItemCount >= sellPrices[item].value;
-  },
+    canBuyItem: function(item){
+      var playerInventoryItemCount = GameData.player[sellPrices[item].type];
+      return playerInventoryItemCount >= sellPrices[item].value;
+    },
 
-  attack: function(enemy){
-    window.currentBattle.eventEngine.add({
-      type:    "player_battle_move",
-      message: "You attacked",
-      perform: window.Animations.Attack.play
-    });
-  },
-
-  calculateDamage: function(){
-    return GameData.player.attack_damage * this.weapon().damage;
-  },
-
-  looseHealth: function(amount){
-    GameData.player.hp[0] -= Math.round(amount);
-    if(GameData.player.hp[0] <= 0){
-      GameData.player.hp[0] = 0;
+    attack: function(enemy){
       window.currentBattle.eventEngine.add({
-        type:    "death",
-        message: "You lost",
-        timeOut: 2000
+        type:    "player_battle_move",
+        message: "You attacked",
+        perform: window.Animations.Attack.play
       });
+    },
 
-      window.currentBattle.eventEngine.add({
-        type: "end",
-        timeOut: 2000
+    calculateDamage: function(){
+      return GameData.player.attack_damage * this.weapon().damage;
+    },
+
+    looseHealth: function(amount){
+      GameData.player.hp[0] -= Math.round(amount);
+      if(GameData.player.hp[0] <= 0){
+        GameData.player.hp[0] = 0;
+        window.currentBattle.eventEngine.add({
+          type:    "death",
+          message: "You lost",
+          timeOut: 2000
+        });
+
+        window.currentBattle.eventEngine.add({
+          type: "end",
+          timeOut: 2000
+        });
+      }
+      this.healthBar.update(GameData.player.hp[0]);
+    },
+
+    increaseHealth: function(amount){
+      GameData.player.hp[0] += amount;
+      if(GameData.player.hp[0] >= GameData.player.hp[1]){
+        GameData.player.hp[0] = GameData.player.hp[1];
+      }
+      this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
+      this.inventory.healthBar.set(GameData.player.hp[0], GameData.player.hp[1])
+    },
+
+    increaseMagic: function(amount){
+      GameData.player.mp[0] += amount;
+      if(GameData.player.mp[0] >= GameData.player.mp[1]){
+        GameData.player.mp[0] = GameData.player.mp[1];
+      }
+      // update magic bar
+    },
+
+    getGraphic: function(){
+      return this.graphic.innerHTML;
+    },
+
+    spawn: function(){
+      this.playerPlaceholder = dom.find("#battle-sequence-popup .field .player #graphic");
+      this.playerPlaceholder.innerHTML = this.getGraphic();
+    },
+
+    unspawn: function(){
+      this.playerPlaceholder.innerHTML = "";
+    },
+
+    setCurrentWeapon: function(weapon){
+      window.GameData.player.weapon = weapon;
+      this.updateWeaponGraphics(weapon);
+    },
+
+    updateWeaponGraphics: function(weapon){
+      var weaponParts = this.graphic.querySelectorAll('.weapon');
+      $.each(window.Weapons[weapon].graphic, function(i, graphicPart){
+        weaponParts[i].innerHTML = graphicPart;
+      });
+      this.inventory.updateGraphicalWeaponPreview(weapon);
+    },
+
+    setCurrentArmor: function(armor){
+      window.GameData.player.armor = armor;
+      this.updateArmorGraphics(armor);
+    },
+
+    armor: function(){
+      return window.Armors[window.GameData.player.armor];
+    },
+
+    weapon: function(){
+      return window.Weapons[window.GameData.player.weapon];
+    },
+
+    updateArmorGraphics: function(armor){
+      var armorParts = this.graphic.querySelectorAll('.armor');
+      $.each(window.Armors[armor].graphic, function(i, graphicPart){
+        armorParts[i].innerHTML = graphicPart;
+      });
+      this.inventory.updateGraphicalArmorPreview(armor);
+    },
+
+    loadCurrentArmor: function(){
+      if(window.GameData.player.armor){
+        this.inventory.setSelectedArmor(window.GameData.player.armor);
+      }
+    },
+
+    loadCurrentWeapon: function(){
+      if(window.GameData.player.weapon){
+        this.inventory.setSelectedWeapon(window.GameData.player.weapon);
+      }
+    },
+
+    prepareForBattle: function(){
+      this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
+      $.each(["skills", "magic", "items"], function(i, scope){
+        dom.find("#battle-sequence-popup ." + scope + " ul.list").innerHTML = "";
+        $.each(GameData.player.inventory[scope], function(i, item){
+          new BattleItem(item, scope).add();
+        });
       });
     }
-    this.healthBar.update(GameData.player.hp[0]);
-  },
-
-  increaseHealth: function(amount){
-    GameData.player.hp[0] += amount;
-    if(GameData.player.hp[0] >= GameData.player.hp[1]){
-      GameData.player.hp[0] = GameData.player.hp[1];
-    }
-    this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
-    this.inventory.healthBar.set(GameData.player.hp[0], GameData.player.hp[1])
-  },
-
-  increaseMagic: function(amount){
-    GameData.player.mp[0] += amount;
-    if(GameData.player.mp[0] >= GameData.player.mp[1]){
-      GameData.player.mp[0] = GameData.player.mp[1];
-    }
-    // update magic bar
-  },
-
-  getGraphic: function(){
-    return this.graphic.innerHTML;
-  },
-
-  spawn: function(){
-    this.playerPlaceholder = dom.find("#battle-sequence-popup .field .player #graphic");
-    this.playerPlaceholder.innerHTML = this.getGraphic();
-  },
-
-  unspawn: function(){
-    this.playerPlaceholder.innerHTML = "";
-  },
-
-  setCurrentWeapon: function(weapon){
-    window.GameData.player.weapon = weapon;
-    this.updateWeaponGraphics(weapon);
-  },
-
-  updateWeaponGraphics: function(weapon){
-    var weaponParts = this.graphic.querySelectorAll('.weapon');
-    $.each(window.Weapons[weapon].graphic, function(i, graphicPart){
-      weaponParts[i].innerHTML = graphicPart;
-    });
-    this.inventory.updateGraphicalWeaponPreview(weapon);
-  },
-
-  setCurrentArmor: function(armor){
-    window.GameData.player.armor = armor;
-    this.updateArmorGraphics(armor);
-  },
-
-  armor: function(){
-    return window.Armors[window.GameData.player.armor];
-  },
-
-  weapon: function(){
-    return window.Weapons[window.GameData.player.weapon];
-  },
-
-  updateArmorGraphics: function(armor){
-    var armorParts = this.graphic.querySelectorAll('.armor');
-    $.each(window.Armors[armor].graphic, function(i, graphicPart){
-      armorParts[i].innerHTML = graphicPart;
-    });
-    this.inventory.updateGraphicalArmorPreview(armor);
-  },
-
-  loadCurrentArmor: function(){
-    if(window.GameData.player.armor){
-      this.inventory.setSelectedArmor(window.GameData.player.armor);
-    }
-  },
-
-  loadCurrentWeapon: function(){
-    if(window.GameData.player.weapon){
-      this.inventory.setSelectedWeapon(window.GameData.player.weapon);
-    }
-  },
-
-  prepareForBattle: function(){
-    this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
-    $.each(["skills", "magic", "items"], function(i, scope){
-      dom.find("#battle-sequence-popup ." + scope + " ul.list").innerHTML = "";
-      $.each(GameData.player.inventory[scope], function(i, item){
-        new BattleItem(item, scope).add();
-      });
-    });
   }
-}
 
-function Player(){
-  this.inventory      = new Inventory();
-  this.graphic        = dom.findId("game-character");
-  this.battleGraphic  = dom.find("#battle-sequence-popup .player #graphic");
-  this.healthBar      = dom.find(".field .player .healthbar .health-left");
-  this.healthStats    = dom.find(".field .player .health-stats .health-stats-left");
-  this.attackBar      = new Bar(".field .player .attackbar .attack-left", GameData.player.battle_timeout);
-  this.healthBar      = new StatBar(".player #health", ".health-stats-left", ".total-health", ".healthbar .inner-bar");
-  //this.magicBar       = new StatBar(".player #magic", ".magic-stats-left", ".total-magic", ".magicbar .inner-bar");
-  this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
-}
+  function Player(){
+    this.inventory      = new Inventory();
+    this.graphic        = dom.findId("game-character");
+    this.battleGraphic  = dom.find("#battle-sequence-popup .player #graphic");
+    this.healthBar      = dom.find(".field .player .healthbar .health-left");
+    this.healthStats    = dom.find(".field .player .health-stats .health-stats-left");
+    this.attackBar      = new Bar(".field .player .attackbar .attack-left", GameData.player.battle_timeout);
+    this.healthBar      = new StatBar(".player #health", ".health-stats-left", ".total-health", ".healthbar .inner-bar");
+    //this.magicBar       = new StatBar(".player #magic", ".magic-stats-left", ".total-magic", ".magicbar .inner-bar");
+    this.healthBar.set(GameData.player.hp[0], GameData.player.hp[1]);
+  }
+  return Player;
+})();
