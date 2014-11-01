b400658f0b546ecdb375a986744d33b401ec7346
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-01 13:33:20 +0100

    adding experience points

--- src/statics/ExperiencePoints.js
+++ src/statics/ExperiencePoints.js
@@ -1,7 +1,7 @@
 window.ExperiencePoints = {
   "9":  [{ callback_method: "increaseHitPoints", amount: 10 }],
   "19": [{ callback_method: "increaseHitPoints", amount: 20 }],
-  "49": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "49": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }]
 };
 
 $.freeze(window.ExperiencePoints);
