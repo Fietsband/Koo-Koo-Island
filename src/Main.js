937961f7f0bb8d8b6dc148fd6b8d2cd474cb472b
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-01 11:08:22 +0100

    moving lots of methods to private scope

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,7 @@
 var Game = (function(){
+  var autoSaveInterval = null;
+  var autoSaveTimeInterval = 5000;
+
   Game.prototype = {
     initialize: function(){
       if($.isBrowserCompatible()){
@@ -7,52 +10,16 @@ var Game = (function(){
         this.gid        = GameStorage.get("koo-koo-island-gid") || $.guid();
         this.player     = new Player();
         this.gameMap    = new GameMap();
-        this.setLevels();
-        this.setCallbacks();
-        this.storeGid();
-        this.getOldGameData();
-        this.toggleAutoSaveInterval();
+
+        setLevels.call(this);
+        loadCurrentLevel.call(this);
+        setCallbacks.call(this);
+        storeGid.call(this);
+        getOldGameData.call(this);
+        toggleAutoSaveInterval.call(this);
       }
       else{
         alert("Your browser is not compatible to play this game.");
-      }
-    },
-
-    setLevels: function(){
-      this.levels = {};
-      var self = this;
-      for(var level in window.Levels){
-        this.levels[level] = new Level(level);
-        $.each(window.Levels[level], function(i, sublevel){
-          self.levels[sublevel] = new Level(sublevel, level);
-        });
-      };
-      window.Levels = null;
-      delete window.Levels;
-
-      this.levels.lonely_island.addToGame();
-    },
-
-    setCallbacks: function(){
-      this.callbacks                      = {};
-      this.callbacks.loadCallbacks        = LoadCallbacks;
-      this.callbacks.experienceCallbacks  = ExperienceCallbacks;
-      this.callbacks.statsCallbacks       = StatsCallbacks;
-
-      if(ENV == "production"){
-        delete LoadCallbacks, ExperienceCallbacks, StatsCallbacks;
-      }
-    },
-
-    toggleAutoSaveInterval: function(){
-      if(GameData.settings.autoSaveEnabled){
-        this.autoSaveInterval = setInterval(
-          this.save.bind(this),
-          this.autoSaveTimeInterval
-        );
-      }
-      else{
-        clearInterval(this.autoSaveInterval);
       }
     },
 
@@ -61,76 +28,107 @@ var Game = (function(){
       GameStorage.store(this.gid, this.getCurrentGame());
     },
 
-    storeGid: function(){
-      if(!GameStorage.keyExists("koo-koo-island-gid")){
-        GameStorage.store("koo-koo-island-gid", this.gid);
-        this.save();
-      }
-    },
-
     loadGame: function(base64_string){
       GameStorage.store(this.gid, base64_string);
       window.GameData = JSON.parse(atob(GameStorage.get(this.gid)));
-      this.toggleLoadCallbacks();
-    },
-
-    getOldGameData: function(){
-      if(ENV == "production"){
-        window.GameData = JSON.parse(atob(GameStorage.get(this.gid))) || {};
-      }
-      this.toggleLoadCallbacks();
+      toggleLoadCallbacks.call(this);
     },
 
     getCurrentGame: function(){
       return btoa(JSON.stringify(window.GameData));
     },
 
-    toggleLoadCallbacks: function(){
-      this.trackCurrentProgress();
-      this.setupStats();
-      this.setupCurrentInventory();
-      this.callbacks.loadCallbacks.setupPlayer();
-    },
-
-    trackCurrentProgress: function(){
-      var self = this;
-      $.each(Object.keys(window.GameData.progress), function(i, progressItem){
-        if(self.checkProgressOn(progressItem)){
-          self.callbacks.loadCallbacks[progressItem]();
-        }
-      });
-    },
-
-    setupCurrentInventory: function(){
-      if(this.player.inventory.hasSomethingInInventory()){
-        this.callbacks.loadCallbacks.setupInventory();
-      }
-    },
-
-    setupStats: function(){
-      var self = this;
-      $.each(["seashell", "oyster", "wood"], function(i, statType){
-        if(window.GameData.player[statType + "s"] > 0){
-          self.callbacks.loadCallbacks.show_stat(statType);
-        }
-      });
-    },
-
     toggleAutoSave: function(){
       GameData.settings.autoSaveEnabled = !GameData.settings.autoSaveEnabled;
-      this.toggleAutoSaveInterval();
+      toggleAutoSaveInterval.call(this);
     },
 
     checkProgressOn: function(part){
       return GameData.progress[part] == 1;
-    },
-
-    autoSaveInterval: null,
-    autoSaveTimeInterval: 5000
+    }
   }
 
-  function createLevel(){
+  function loadCurrentLevel(){
+    this.levels[GameData.player.last_visited].addToGame();
+  }
 
+  function setLevels(){
+    this.levels = {};
+    var self = this;
+    for(var level in window.Levels){
+      this.levels[level] = new Level(level);
+      $.each(window.Levels[level], function(i, sublevel){
+        self.levels[sublevel] = new Level(sublevel, level);
+      });
+    };
+  }
+
+  function setCallbacks(){
+    this.callbacks                      = {};
+    this.callbacks.loadCallbacks        = LoadCallbacks;
+    this.callbacks.experienceCallbacks  = ExperienceCallbacks;
+    this.callbacks.statsCallbacks       = StatsCallbacks;
+
+    if(ENV == "production"){
+      delete LoadCallbacks, ExperienceCallbacks, StatsCallbacks;
+    }
+  }
+
+  function storeGid(){
+    if(!GameStorage.keyExists("koo-koo-island-gid")){
+      GameStorage.store("koo-koo-island-gid", this.gid);
+      this.save();
+    }
+  }
+
+  function getOldGameData(){
+    if(ENV == "production"){
+      window.GameData = JSON.parse(atob(GameStorage.get(this.gid))) || {};
+    }
+    toggleLoadCallbacks.call(this);
+  }
+
+  function toggleAutoSaveInterval(){
+    if(GameData.settings.autoSaveEnabled){
+      autoSaveInterval = setInterval(
+        this.save.bind(this),
+        autoSaveTimeInterval
+      );
+    }
+    else{
+      clearInterval(autoSaveInterval);
+    }
+  }
+
+  function toggleLoadCallbacks(){
+    trackCurrentProgress.call(this);
+    setupStats.call(this);
+    setupCurrentInventory.call(this);
+    this.callbacks.loadCallbacks.setupPlayer();
+  }
+
+  function trackCurrentProgress(){
+    var self = this;
+    $.each(Object.keys(window.GameData.progress), function(i, progressItem){
+      if(self.checkProgressOn(progressItem)){
+        self.callbacks.loadCallbacks[progressItem]();
+      }
+    });
+  }
+
+  function setupCurrentInventory(){
+    if(this.player.inventory.hasSomethingInInventory()){
+      this.callbacks.loadCallbacks.setupInventory();
+    }
+  }
+
+  function setupStats(){
+    var self = this;
+    $.each(["seashell", "oyster", "wood"], function(i, statType){
+      if(window.GameData.player[statType + "s"] > 0){
+        self.callbacks.loadCallbacks.show_stat(statType);
+      }
+    });
   }
 
   function Game(){}
