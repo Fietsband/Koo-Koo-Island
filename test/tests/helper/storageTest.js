5e6a82322101e24baa8a0bfd9adec7c7c15e0032
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-01 11:31:11 +0100

    removing storage test

--- test/tests/helper/storageTest.js
+++ test/tests/helper/storageTest.js
@@ -1,7 +1,3 @@
-QUnit.test("should pass the game storage test or else the game won't load", function(assert){
-  assert.equal(GameStorage.canStore(), true, "Can store!")
-})
-
 QUnit.test("should be able to store some data", function(assert){
   GameStorage.store("test", "test");
 
