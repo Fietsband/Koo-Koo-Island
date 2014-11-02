907323faa963c3e1699f9662d517c36cd7e4aa31
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-02 12:07:56 +0100

    rewrite experience callbacks

--- src/callbacks/ExperienceCallbacks.js
+++ src/callbacks/ExperienceCallbacks.js
@@ -1,40 +1,46 @@
-ExperienceCallbacks = {
-  increaseExperience: function(amount){
-    GameData.player.experience += amount;
-    ExperienceCallbacks.checkExperiencePoints();
-  },
+var ExperienceCallbacks = (function(){
+  ExperienceCallbacks = {
+    increaseExperience: function(amount){
+      GameData.player.experience += amount;
+      checkExperiencePoints();
+    }
+  };
 
-  increaseHitPoints: function(amount){
-    GameData.player.hp[1] += amount;
-  },
-
-  increaseMagicPoints: function(amount){
-    GameData.player.mp[1] += amount;
-  },
-
-  checkExperiencePoints: function(){
-    $.each(window.currentGame.callbacks.experienceCallbacks.experienceCallbacks(), function(i, experienceKey){
+  function checkExperiencePoints(){
+    $.each(experienceCallbacks(), function(i, experienceKey){
       if(GameData.player.experience > parseInt(experienceKey) &&
         GameData.player.experience_rewards.indexOf(experienceKey) === -1){
-        window.currentGame.callbacks.experienceCallbacks.experiencePointReward(experienceKey);
+        experiencePointReward(experienceKey);
       }
     });
-  },
+  }
 
-  experiencePointReward: function(experienceKey){
-    GameData.player.experience_rewards.push(experienceKey);
-    $.each(ExperiencePoints[experienceKey], function(j, callback){
-      window.currentBattle.eventEngine.add({
-        perform: window.currentGame.callbacks.experienceCallbacks[callback.callback_method](callback.amount),
-        message: "You leveled up!",
-        timeOut: 2000
-      });
-    });
-  },
-
-  experienceCallbacks: function(){
+  function experienceCallbacks(){
     return Object.keys(ExperiencePoints).filter(function(i){
       return parseInt(i) <= GameData.player.experience;
     });
   }
-}
+
+  function experiencePointReward(experienceKey){
+    GameData.player.experience_rewards.push(experienceKey);
+    $.each(ExperiencePoints[experienceKey], function(j, callback){
+      window.currentBattle.eventEngine.add({
+        perform: increase[callback.callback_method](callback.amount),
+        message: "You leveled up!",
+        timeOut: 2000
+      });
+    });
+  }
+
+  var increase = {
+    increaseHitPoints: function(amount){
+      GameData.player.hp[1] += amount;
+    },
+
+    increaseMagicPoints: function(amount){
+      GameData.player.mp[1] += amount;
+    }
+  }
+
+  return ExperienceCallbacks;
+})();
