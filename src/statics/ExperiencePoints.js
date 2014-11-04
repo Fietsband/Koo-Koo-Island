17264ffecbf659867ec0ffacfb72c50febe5d81d
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-04 08:29:38 +0100

    updating experience points

--- src/statics/ExperiencePoints.js
+++ src/statics/ExperiencePoints.js
@@ -1,7 +1,29 @@
 window.ExperiencePoints = {
-  "9":  [{ callback_method: "increaseHitPoints", amount: 10 }],
-  "19": [{ callback_method: "increaseHitPoints", amount: 20 }],
-  "49": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }]
+  "9":   [{ callback_method: "increaseHitPoints", amount: 10 }],
+  "19":  [{ callback_method: "increaseHitPoints", amount: 20 }],
+  "49":  [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }],
+  "99":  [{ callback_method: "increaseSpeed", amount: 500 }]
+  "149": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "199": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }]
+  "249": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "299": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "349": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
+  "399": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "449": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "499": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
+  "549": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "599": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "649": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
+  "699": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "749": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "799": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 100 }]
+  "849": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "899": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "949": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 100 }]
+  "999": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "1249": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "1499": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 200 }]
+  "1749": [{ callback_method: "increaseHitPoints", amount: 20 }]
 };
 
 $.freeze(window.ExperiencePoints);
