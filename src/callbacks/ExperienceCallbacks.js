32eb674a3875c9b36051d5088c187c707e129c47
Gerard Westerhof <gerardwesterhof2@gmail.com>
2014-11-05 08:19:24 +0100

    refactoring experience points

--- src/callbacks/ExperienceCallbacks.js
+++ src/callbacks/ExperienceCallbacks.js
@@ -23,22 +23,27 @@ var ExperienceCallbacks = (function(){
 
   function experiencePointReward(experienceKey){
     GameData.player.experience_rewards.push(experienceKey);
-    $.each(ExperiencePoints[experienceKey], function(j, callback){
+    var experienceReward = ExperiencePoints[experienceKey];
+    for(var experienceUpgrade in experienceReward){
       window.currentBattle.eventEngine.add({
-        perform: increase[callback.callback_method](callback.amount),
+        perform: increase[experienceUpgrade](experienceReward[experienceUpgrade]),
         message: "You leveled up!",
         timeOut: 2000
       });
-    });
+    }
   }
 
   var increase = {
-    increaseHitPoints: function(amount){
+    hp: function(amount){
       GameData.player.hp[1] += amount;
     },
 
-    increaseMagicPoints: function(amount){
+    mp: function(amount){
       GameData.player.mp[1] += amount;
+    },
+
+    speed: function(amount){
+      GameData.player.battle_timeout += amount;
     }
   }
 
--- src/statics/ExperiencePoints.js
+++ src/statics/ExperiencePoints.js
@@ -1,29 +1,29 @@
 window.ExperiencePoints = {
-  "9":   [{ callback_method: "increaseHitPoints", amount: 10 }],
-  "19":  [{ callback_method: "increaseHitPoints", amount: 20 }],
-  "49":  [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }],
-  "99":  [{ callback_method: "increaseSpeed", amount: 500 }]
-  "149": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "199": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 20 }]
-  "249": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "299": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "349": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
-  "399": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "449": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "499": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
-  "549": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "599": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "649": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 50 }]
-  "699": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "749": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "799": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 100 }]
-  "849": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "899": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "949": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 100 }]
-  "999": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "1249": [{ callback_method: "increaseHitPoints", amount: 20 }]
-  "1499": [{ callback_method: "increaseHitPoints", amount: 20 }, { callback_method: "increaseMagicPoints", amount: 200 }]
-  "1749": [{ callback_method: "increaseHitPoints", amount: 20 }]
+  "9":   { hp: 10 },
+  "19":  { hp: 20 },
+  "49":  { hp: 20, mp: 20 },
+  "99":  { speed: 500 },
+  "149": { hp: 20 },
+  "199": { hp: 20, mp: 20 },
+  "249": { hp: 20 },
+  "299": { hp: 20 },
+  "349": { hp: 20, mp: 50 },
+  "399": { hp: 20 },
+  "449": { hp: 20 },
+  "499": { hp: 20, mp: 50 },
+  "549": { hp: 20 },
+  "599": { hp: 20 },
+  "649": { hp: 20, mp: 50 },
+  "699": { hp: 20 },
+  "749": { hp: 20 },
+  "799": { hp: 20, mp: 100 },
+  "849": { hp: 20 },
+  "899": { hp: 20 },
+  "949": { hp: 20, mp: 100 },
+  "999": { hp: 20 },
+  "1249": { hp: 20 },
+  "1499": { hp: 20, mp: 200 },
+  "1749": { hp: 20 }
 };
 
 $.freeze(window.ExperiencePoints);
