5a1ebb707704fae76832ecf30ec2e20385741896
Gerard Westerhof <gerard.westerhof@holder.nl>
2015-01-20 17:24:13 +0100

    setting map to 1

--- src/statics/GameData.js
+++ src/statics/GameData.js
@@ -12,7 +12,7 @@ var GameData = {
     woods:            0,
     armor:            'none',
     weapon:           'none',
-    map:              0,
+    map:              1,
     inventory:        {
       weapons:          [],
       armors:           [],
