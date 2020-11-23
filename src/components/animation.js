65fa6bf4e2e43392da1897c9d76bda8da4af2b03
grdw <gerard@grdw.nl>
2020-11-23 20:02:05 +0100

    Fix imports

--- src/components/animation.js
+++ src/components/animation.js
@@ -1,7 +1,7 @@
 import { FishAnimation } from './animation/fishAnimation.js';
-import { ShootAttackAnimation } from './animation/ShootAttackAnimation.js';
-import { SplashAttackAnimation } from './animation/SplashAttackAnimation.js';
-import { TackleAttackAnimation } from './animation/TackleAttackAnimation.js';
+import { ShootAttackAnimation } from './animation/shootAttackAnimation.js';
+import { SplashAttackAnimation } from './animation/splashAttackAnimation.js';
+import { TackleAttackAnimation } from './animation/tackleAttackAnimation.js';
 
 export const Animation = (function () {
   Animation.prototype = {
